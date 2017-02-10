2137e20cc09fa99ef9cf7fcca56badb24496514b
André Freitas <andre.freitas@ndrive.com>
2017-02-10 00:22:10 +0000

    Solves roman numerals ruby exercise

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,30 +1,34 @@
-require "pry"
-
 class Integer
 
+  ROMANS = {
+    1000 => "M",
+    900 => "CM",
+    500 => "D",
+    400 => "CD",
+    100 => "C",
+    90 => "XC",
+    50 => "L",
+    40 => "XL",
+    10 => "X",
+    9 => "IX",
+    5 => "V",
+    4 => "IV",
+    1 => "I"
+   }
+
    def to_roman
-    numerals = {
-       1 => "I",
-       4 => "IV",
-       5 => "V",
-       10 => "X",
-       50 => "L",
-       100 => "C",
-       500 => "D",
-       1000 => "M"
-     }
+    number = self
+    result = ""
 
-     number = self
-     romans = []
+    ROMANS.each do |numeral, roman|
+      result << roman * (number / numeral)
+      number %= numeral
+    end
 
-     numerals.sort.reverse.each do |factor, roman|
-
-      #TODO: Fix VI test case
-      while number > 0 && number % factor == 0
-        romans.push roman
-        number -= factor
-      end
-     end
-     romans.join
+    result
    end
+end
+
+class BookKeeping
+  VERSION = 2
 end
