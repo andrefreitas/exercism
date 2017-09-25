25436e227e9bc28bbe386d853a85a18c89ef0a51
André Freitas <p.andrefreitas@gmail.com>
2017-09-25 10:20:39 +0100

    Solves Kotlin RNA Transcription

---
+++ kotlin/rna-transcription/src/main/kotlin/RnaTranscription.kt
@@ -0,0 +1,10 @@
+val dnaMap = mapOf(
+  'G' to 'C',
+  'C' to 'G',
+  'T' to 'A',
+  'A' to 'U'
+)
+
+fun transcribeToRna(dna: String): String {
+  return dna.map{c -> dnaMap[c]}.joinToString("")
+}
