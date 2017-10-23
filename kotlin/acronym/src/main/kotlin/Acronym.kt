027e35efa33a7988bd6fc61867bce22ca3650ce0
André Freitas <p.andrefreitas@gmail.com>
2017-10-23 15:28:43 +0100

    Compacts kotlin/acronym solution

--- kotlin/acronym/src/main/kotlin/Acronym.kt
+++ kotlin/acronym/src/main/kotlin/Acronym.kt
@@ -1,14 +1,8 @@
 object Acronym {
   fun generate(phrase: String): String {
-    return words(phrase)
-            .map{it.first()}
-            .joinToString("")
-            .toUpperCase()
-  }
-
-  fun words(phrase: String): Sequence<String> {
     return Regex("([A-Z]+[a-z]*)|[a-z]+")
             .findAll(phrase)
-            .map{it.value}
+            .map{it.value.first().toUpperCase()}
+            .joinToString("")
   }
 }
